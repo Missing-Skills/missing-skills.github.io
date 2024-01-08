071fd2337e97177fa4d95abfa3dc7ab89d220a7e
Ben Burns <bburns@umass.edu>
2024-01-08 15:39:17 -0500

    chore: update index description

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,11 +11,11 @@ function Feature() {
           computer science curriculum. Advanced computer literacy is an essential skill that makes students more
           prepared, versatile, and productive. During this course, students will gain experience using the command line,
           version control, package managers, scripting languages, LaTeX, code editors, and more. Students will engage
-          with weekly material during class under the supervision and leadership of upper-classmen. After class,
-          students are expected to complete a small take-home activity to assess their resourcefulness and understanding
+          with material under the supervision and leadership of upperclassmen. For each unit,
+          students are expected to complete a small take-home activity to bolster their understanding
           of the material. After taking this course, students will have a breadth of new skills that will help them
           succeed in future coursework and industry positions. Intended for undergraduate computer science and
-          informatics majors. Prerequisites: CICS 110, INFO 190S, or equivalent programming experience. 1 credit.
+          informatics majors. Expected background: CICS 110, INFO 190S, or equivalent programming experience.
         </p>
       </div>
     </div>
